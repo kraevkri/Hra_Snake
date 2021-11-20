dd46c5937dff7fd0ca215cc6b493d1ff8d62c0da
Kristian Kraevski <91134071+kraevkri@users.noreply.github.com>
2021-11-20 19:17:31 +0100

    Update Had.cpp

--- Had.cpp
+++ Had.cpp
@@ -1,8 +1,8 @@
 #include "Had.h"
-
-Had::Had(COORD poz, int rychlost)
+#include<conio.h>
+Had::Had(COORD pozice, int rychlost)
 {
-    this->poz=poz;
+    this->pozice=pozice;
     this->rychlost=rychlost;
     delka=1;
     smerHada='n';
@@ -15,13 +15,27 @@ void Had::pohniHada()
 {
     switch(smerHada)
         {
-            case 's': poz.Y -=rychlost; break;
-            case 'j': poz.Y +=rychlost; break;
-            case 'z': poz.X -=rychlost; break;
-            case 'v': poz.X +=rychlost; break;
+            case 's': pozice.Y -=rychlost; break;
+            case 'j': pozice.Y +=rychlost; break;
+            case 'z': pozice.X -=rychlost; break;
+            case 'v': pozice.X +=rychlost; break;
 
         }
 }
 
 
-COORD Had::ziskatPozici() {return poz;}
+COORD Had::ziskatPozici() {return pozice;}
+
+bool Had::snezeno(COORD poziceJidla)
+{
+    if(poziceJidla.X==pozice.X&&poziceJidla.Y==pozice.Y) return true;
+    else return false;
+}
+
+void Had::rust() {delka++;}
+
+bool Had::narazil()
+{
+    if(pozice.X<1||pozice.X>sirka-1||pozice.Y<1||pozice.Y>vyska-1) return true;
+    return false;
+}

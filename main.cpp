468c788f5b47cbfd1791463fb9bbcab80523241e
Kristian Kraevski <91134071+kraevkri@users.noreply.github.com>
2021-11-20 19:16:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,44 +1,62 @@
 #include <iostream>
-#include <cstdlib>
 #include <conio.h>
+#include <ctime>
 #include "Had.h"
+#include "Jidlo.h"
 // MAKROS
 
 #define sirka 50
 #define vyska 25
 
-// test test
+
 
 
 using namespace std;
 
 Had had({sirka/2,vyska/2}, 1);
+Jidlo jidlo;
 
+int body;
 
 void plocha()
 {
 
     COORD poziceHada=had.ziskatPozici();
+    COORD poziceJidla=jidlo.ziskatPozici();
 
     for(int i=0; i<vyska; i++)
     {
-        cout<<"\t\t\t#";
+        cout<<"\t\t\t|";
+
         for(int j=0;j<sirka-2; j++)
         {
-            if(i==0||i==vyska-1) cout<<'#';
-            else if(i==poziceHada.Y && j==poziceHada.X) cout<<'o';
+            if(i==0||i==vyska-1) cout<<'_';
+
+            else if(i==poziceHada.Y && j+1==poziceHada.X) cout<<'o';
+            else if(i==poziceJidla.Y&&j+1==poziceJidla.X) cout<<'@';
+
             else cout<<' ';
         }
-        cout<<"#\n";
+        cout<<"|\n";
     }
 }
 
 
 int main()
 {
-    while(true)
+    body=0;
+
+
+    srand(time(NULL));
+
+    jidlo.generujJidlo();
+
+    char konecHry = false;
+
+    while(!konecHry)
     {
         plocha();
+
 
         if(kbhit())
         {
@@ -49,6 +67,21 @@ int main()
                 case 'd': had.zmenaSmeru('v'); break;
             }
         }
+
+        if(had.narazil())
+                {
+                konecHry=true;
+
+                cout<<"\n\n\n\t\t\tGAME OVER\n\t\t\tbody="; cout<<body;
+                }
+
+        if(had.snezeno(jidlo.ziskatPozici()))
+        {
+            jidlo.generujJidlo();
+            had.rust();
+            body=body+10;
+        }
+
         had.pohniHada();
 
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),{0,0});

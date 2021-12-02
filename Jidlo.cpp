3707a98421cd215919211af749a18561bdcd2d6c
vasek321 <91741841+vasek321@users.noreply.github.com>
2021-12-02 19:17:12 +0100

    Update Jidlo.cpp

--- Jidlo.cpp
+++ Jidlo.cpp
@@ -1,11 +1,12 @@
 #include "Jidlo.h"
 
+char Jidlo::jidlochar = 0;
 
 void Jidlo::generujJidlo()
 
 {
-    pozice.X=(rand() % sirka-3) +3;
-    pozice.Y=(rand() % vyska-3) +3;
+    pozice.X=(rand() % (sirka-2)) +1; //-3 a +3 by mìlo fixnout mizení generování jídla z mapy
+    pozice.Y=(rand() % (vyska-2)) +1;
 }
 
 COORD Jidlo::ziskatPozici(){return pozice;}

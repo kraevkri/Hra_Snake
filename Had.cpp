a11fb83312ad3196e5d1d2c60513146944312278
vasek321 <91741841+vasek321@users.noreply.github.com>
2021-12-05 16:51:41 +0100

    Update Had.cpp

--- Had.cpp
+++ Had.cpp
@@ -4,44 +4,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-char Had::hadchar =0;
+#define sleeptime 100000
+
+char Had::hadchar;
 
 Had::Had(COORD pozice, int rychlost)
 {
-    this->pozice=pozice;
-    this->rychlost=rychlost;
-    delka=1;
-    smerHada='n';
+    this->pozice = pozice;
+    this->rychlost = rychlost;
+    delka = 1;
+    smerHada = 'n';
 
-    telo.push_back(pozice); //pøidá symbol do tela
-
+    telo.push_back(pozice); //prida symbol do tela
 }
 
-void Had::zmenaSmeru(char smer){smerHada=smer;} // funkce void nevrací žádnou hodnotu
+void Had::zmenaSmeru(char smer){smerHada = smer;} // funkce void nevraci zadnou hodnotu
 
-vector<COORD> Had::ziskat_telo() { return telo; }
+vector <COORD> Had::ziskat_telo()
+{
+    return telo;
+}
 
 void Had::pohniHada()
 {
     switch(smerHada) //zjednoduseny podminky if,else
         {
-            case 's': pozice.Y -=rychlost;sleep(0.99999999); break; //nahoru //break používá se u switch a okamžitì ukonèí aktualni cyklus
-            case 'j': pozice.Y +=rychlost;sleep(0.99999999); break; //dolu
-            case 'z': pozice.X -=rychlost;sleep(0.4); break; //doleva
-            case 'v': pozice.X +=rychlost;sleep(0.4); break; //doprava
-
+            case 's': pozice.Y -= rychlost; usleep(sleeptime*0.5); break; //nahoru //break pouziva se u switch a okamzite ukonci aktualni cyklus
+            case 'j': pozice.Y += rychlost; usleep(sleeptime*0.5); break; //dolu
+            case 'z': pozice.X -= rychlost; usleep(sleeptime*0.2); break; //doleva
+            case 'v': pozice.X += rychlost; usleep(sleeptime*0.2); break; //doprava
         }
 
         telo.push_back(pozice); //znova po nejakej cyklu
         if(telo.size() > delka) telo.erase(telo.begin()); //rust hada
 }
 
-
-COORD Had::ziskatPozici() {return pozice;} // had:: - je tam protože je ta funkce definovana v souboru had
+COORD Had::ziskatPozici()
+{
+    return pozice;
+} // had:: - je tam protože je ta funkce definovana v souboru had
 
 bool Had::snezeno(COORD poziceJidla)
 {
-    if(poziceJidla.X==pozice.X&&poziceJidla.Y==pozice.Y) return true;
+    if(poziceJidla.X==pozice.X && poziceJidla.Y==pozice.Y) return true;
     else return false;
 }
 
@@ -49,6 +54,6 @@ void Had::rust() {delka++;}
 
 bool Had::narazil() //bool podmínka do které lze uložit pouze true/false
 {
-    if(pozice.X<1||pozice.X>sirka-1||pozice.Y<1||pozice.Y>vyska-1) return true;
+    if(pozice.X<1 || pozice.X>sirka-1 || pozice.Y<1 || pozice.Y>vyska-1) return true;
     return false;
 }
